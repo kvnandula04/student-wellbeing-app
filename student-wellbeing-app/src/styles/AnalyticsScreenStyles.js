9e916cf5a61758a50b3f36b19def5c7c16b1f6c9
Karthik Nandula <kvnandula04@gmail.com>
2022-03-17 23:47:00 +0000

    Created new style for analytics pages

---
+++ student-wellbeing-app/src/styles/AnalyticsScreenStyles.js
@@ -0,0 +1,37 @@
+import { StyleSheet } from "react-native";
+import colors from "./Colors";
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.PRIMARYCOLOR,
+  },
+  input: {
+    marginHorizontal: "5%",
+    marginTop: "5%",
+    padding: "3%",
+    borderWidth: 0.5,
+    borderColor: "grey",
+    borderRadius: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  button: {
+    backgroundColor: colors.CARDCOLOR,
+    marginHorizontal: "5%",
+    marginTop: "5%",
+    marginBottom: "20%",
+    padding: "3%",
+    borderRadius: 10,
+    justifyContent: "center",
+  },
+  text: {
+    fontWeight: "bold",
+    textAlign: "center",
+    textTransform: "uppercase",
+  },
+  analyticstext: {
+    marginBottom: "4%",
+    marginLeft: "5%",
+  },
+});
