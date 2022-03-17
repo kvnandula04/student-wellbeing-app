d01b9154cf98d3965362ccd39231828cf0870bb1
Karthik Nandula <kvnandula04@gmail.com>
2022-03-17 23:46:41 +0000

    Added ScrollView for smaller screen sizes

--- student-wellbeing-app/src/screens/AnalyticsScreens/FoodAnalytics.js
+++ student-wellbeing-app/src/screens/AnalyticsScreens/FoodAnalytics.js
@@ -1,22 +1,18 @@
 import React from "react";
 import PlaceholderStyles from "../../styles/PlaceholderStyles";
 import {
-  SafeAreaView,
-  TextInput,
   Image,
   StyleSheet,
   Text,
   View,
-  Button,
-  Alert,
   Pressable,
-  Dimensions
+  Dimensions,
+  ScrollView,
 } from "react-native";
-import { CustomCard } from "../../components/CustomCard";
-import { CARDCOLOR, PRIMARYCOLOR } from "../../styles/Constants";
+import { EmptyCard } from "../../components/EmptyCard";
+import AnalyticsScreenStyles from "../../styles/AnalyticsScreenStyles";
+import colors from "../../styles/Colors";
 import { LineChart } from "react-native-chart-kit";
-
-
 
 const screenWidth = Dimensions.get("window").width;
 const backendvalue1 = "Tuesday";
@@ -25,121 +21,100 @@ const backendvalue3 = "6/10";
 
 const Stat = (props) => {
   return (
-      <Text style={{textAlign: "right", fontWeight: "bold",}}>{props.name}</Text>
+    <Text style={{ textAlign: "right", fontWeight: "bold" }}>{props.name}</Text>
   );
-}
+};
 
-export default function FoodAnalytics({navigation}) {
-    const data = {
-        labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
-        datasets: [{
-            lineTension: 0.5,
-            borderWidth: 2,
-            data: [1,3,2,4,7,6,9]
-    
-        }]
-    }
-    
-    return(
-        <>
-        <View style = {styles.container}>
-        <CustomCard
-        elevated={true}
-        style={{
-          backgroundColor: CARDCOLOR,
-          marginHorizontal: "5%",
-          marginTop: "20%",
-          padding: "7%",
-          borderRadius: 10,
-          flexDirection: "row",
-          justifyContent: "space-between",
-        }}
-      >
-        <View style={{ alignItems: "flex-start", flexDirection: "row" }}>
-          <Image
-            source={require("../../assets/food.png")}
-            style={{
-              width: 130 * 0.6,
-              height: 122 * 0.6,
-            }}
-          />
-          <View>
-            <Text
-              style={{ fontWeight: "bold", marginLeft: "20%", fontSize: 30 }}
-            >
-              Food
-            </Text>
-            <Text
-              style={{ fontWeight: "bold", marginLeft: "22%", fontSize: 12 }}
-            >
-              Analytics
-            </Text>
-          </View>
-        </View>
-      </CustomCard>
-      <View style={PlaceholderStyles.container}>
-        <LineChart
-          data={data}
-          width={screenWidth * 0.9}
-          height={250}
-          chartConfig={{
-            backgroundGradientFrom: PRIMARYCOLOR,
-            backgroundGradientTo: PRIMARYCOLOR,
-            decimalPlaces: 0, 
-            color: (opacity = 255) => 'black'
+export default function FoodAnalytics({ navigation }) {
+  const data = {
+    labels: ["M", "T", "W", "T", "F", "S", "S"],
+    datasets: [
+      {
+        lineTension: 0.5,
+        borderWidth: 2,
+        data: [1, 3, 2, 4, 7, 6, 9],
+      },
+    ],
+  };
+
+  return (
+    <ScrollView>
+      <View style={AnalyticsScreenStyles.container}>
+        <EmptyCard
+          elevated={true}
+          style={{
+            marginHorizontal: "5%",
+            marginTop: "20%",
+            padding: "7%",
+            justifyContent: "space-between",
           }}
-          style={{marginTop: "10%", marginRight: 5, borderRadius: 10}}
-        />
-        <Text style ={{textAlign: "center", marginBottom: "8%", fontSize: 8, fontWeight: "bold",}}> Day </Text>
+        >
+          <View style={{ alignItems: "flex-start", flexDirection: "row" }}>
+            <Image
+              source={require("../../assets/food.png")}
+              style={{
+                width: 130 * 0.6,
+                height: 122 * 0.6,
+              }}
+            />
+            <View>
+              <Text
+                style={{ fontWeight: "bold", marginLeft: "20%", fontSize: 30 }}
+              >
+                Food
+              </Text>
+              <Text
+                style={{ fontWeight: "bold", marginLeft: "22%", fontSize: 12 }}
+              >
+                Analytics
+              </Text>
+            </View>
+          </View>
+        </EmptyCard>
+        <View style={PlaceholderStyles.container}>
+          <LineChart
+            data={data}
+            width={screenWidth * 0.9}
+            height={250}
+            chartConfig={{
+              backgroundGradientFrom: colors.PRIMARYCOLOR,
+              backgroundGradientTo: colors.PRIMARYCOLOR,
+              decimalPlaces: 0,
+              color: (opacity = 255) => "black",
+            }}
+            style={{ marginTop: "10%", marginRight: 5, borderRadius: 10 }}
+          />
+          <Text
+            style={{
+              textAlign: "center",
+              marginBottom: "8%",
+              fontSize: 8,
+              fontWeight: "bold",
+            }}
+          >
+            {" "}
+            Day{" "}
+          </Text>
         </View>
         <View>
-        <Text style = {styles.analyticstext}>Most calories on:  <Stat name={backendvalue1} /></Text>
-        <Text style = {styles.analyticstext}>Today's calories: <Stat name={backendvalue2}/></Text>
-        <Text style = {styles.analyticstext}>This week's score: <Stat name={backendvalue3}/></Text>
-      </View>
-      <Pressable
-          style={styles.button}
+          <Text style={AnalyticsScreenStyles.analyticstext}>
+            Most calories on: <Stat name={backendvalue1} />
+          </Text>
+          <Text style={AnalyticsScreenStyles.analyticstext}>
+            Today's calories: <Stat name={backendvalue2} />
+          </Text>
+          <Text style={AnalyticsScreenStyles.analyticstext}>
+            This week's score: <Stat name={backendvalue3} />
+          </Text>
+        </View>
+        <Pressable
+          style={AnalyticsScreenStyles.button}
           onPress={() => navigation.navigate("LogFood")}
         >
-          <Text style={styles.text}>
-            {"BACK"}
-          </Text>
+          <Text style={AnalyticsScreenStyles.text}>{"BACK"}</Text>
         </Pressable>
       </View>
-      </>
-    )
+    </ScrollView>
+  );
 }
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: PRIMARYCOLOR,
-    },
-    input: {
-      marginHorizontal: "5%",
-      marginTop: "5%",
-      padding: "3%",
-      borderWidth: 0.5,
-      borderColor: 'grey',
-      borderRadius: 10,
-      flexDirection: "row",
-      justifyContent: "space-between",
-    },
-    button: {
-      backgroundColor: CARDCOLOR,
-      marginHorizontal: "5%",
-      marginTop: "5%",
-      marginBottom: "20%",
-      padding: "3%",
-      borderRadius: 10,
-      justifyContent: "center",
-    },
-    text: {
-      fontWeight: "bold", 
-      textAlign: "center", 
-      textTransform: 'uppercase'
-    },
-    analyticstext: {
-      marginBottom: "4%",
-      marginLeft: "5%",
-    }
-  });
+const styles = StyleSheet.create({});
--- student-wellbeing-app/src/screens/AnalyticsScreens/SportAnalytics.js
+++ student-wellbeing-app/src/screens/AnalyticsScreens/SportAnalytics.js
@@ -1,21 +1,18 @@
 import React from "react";
 import PlaceholderStyles from "../../styles/PlaceholderStyles";
 import {
-  SafeAreaView,
-  TextInput,
   Image,
   StyleSheet,
   Text,
   View,
-  Button,
-  Alert,
   Pressable,
-  Dimensions
+  Dimensions,
+  ScrollView,
 } from "react-native";
-import { CustomCard } from "../../components/CustomCard";
-import { CARDCOLOR, PRIMARYCOLOR } from "../../styles/Constants";
+import { EmptyCard } from "../../components/EmptyCard";
+import AnalyticsScreenStyles from "../../styles/AnalyticsScreenStyles";
+import colors from "../../styles/Colors";
 import { LineChart } from "react-native-chart-kit";
-
 
 const screenWidth = Dimensions.get("window").width;
 const backendvalue1 = "Tuesday";
@@ -24,121 +21,100 @@ const backendvalue3 = "8" + "hrs";
 
 const Stat = (props) => {
   return (
-      <Text style={{textAlign: "right", fontWeight: "bold",}}>{props.name}</Text>
+    <Text style={{ textAlign: "right", fontWeight: "bold" }}>{props.name}</Text>
   );
-}
+};
 
-export default function SportAnalytics({navigation}) {
-    const data = {
-        labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
-        datasets: [{
-            lineTension: 0.5,
-            borderWidth: 2,
-            data: [1,3,2,4,7,6,9]
-    
-        }]
-    }
-    
-    return(
-        <>
-        <View style = {styles.container}>
-        <CustomCard
-        elevated={true}
-        style={{
-          backgroundColor: CARDCOLOR,
-          marginHorizontal: "5%",
-          marginTop: "20%",
-          padding: "7%",
-          borderRadius: 10,
-          flexDirection: "row",
-          justifyContent: "space-between",
-        }}
-      >
-        <View style={{ alignItems: "flex-start", flexDirection: "row" }}>
-          <Image
-            source={require("../../assets/tennis.png")}
-            style={{
-              width: 68 * 0.6,
-              height: 114 * 0.6,
-            }}
-          />
-          <View>
-            <Text
-              style={{ fontWeight: "bold", marginLeft: "20%", fontSize: 30 }}
-            >
-              Sport
-            </Text>
-            <Text
-              style={{ fontWeight: "bold", marginLeft: "22%", fontSize: 12 }}
-            >
-              Analytics
-            </Text>
-          </View>
-        </View>
-      </CustomCard>
-      <View style={PlaceholderStyles.container}>
-        <LineChart
-          data={data}
-          width={screenWidth * 0.9}
-          height={250}
-          chartConfig={{
-            backgroundGradientFrom: PRIMARYCOLOR,
-            backgroundGradientTo: PRIMARYCOLOR,
-            decimalPlaces: 0, 
-            color: (opacity = 255) => 'black'
+export default function SportAnalytics({ navigation }) {
+  const data = {
+    labels: ["M", "T", "W", "T", "F", "S", "S"],
+    datasets: [
+      {
+        lineTension: 0.5,
+        borderWidth: 2,
+        data: [1, 3, 2, 4, 7, 6, 9],
+      },
+    ],
+  };
+
+  return (
+    <ScrollView>
+      <View style={AnalyticsScreenStyles.container}>
+        <EmptyCard
+          elevated={true}
+          style={{
+            marginHorizontal: "5%",
+            marginTop: "20%",
+            padding: "7%",
+            justifyContent: "space-between",
           }}
-          style={{marginTop: "10%", marginRight: 5, borderRadius: 10}}
-        />
-        <Text style ={{textAlign: "center", marginBottom: "8%", fontSize: 8, fontWeight: "bold",}}> Day </Text>
+        >
+          <View style={{ alignItems: "flex-start", flexDirection: "row" }}>
+            <Image
+              source={require("../../assets/tennis.png")}
+              style={{
+                width: 68 * 0.6,
+                height: 114 * 0.6,
+              }}
+            />
+            <View>
+              <Text
+                style={{ fontWeight: "bold", marginLeft: "20%", fontSize: 30 }}
+              >
+                Sport
+              </Text>
+              <Text
+                style={{ fontWeight: "bold", marginLeft: "22%", fontSize: 12 }}
+              >
+                Analytics
+              </Text>
+            </View>
+          </View>
+        </EmptyCard>
+        <View style={PlaceholderStyles.container}>
+          <LineChart
+            data={data}
+            width={screenWidth * 0.9}
+            height={250}
+            chartConfig={{
+              backgroundGradientFrom: colors.PRIMARYCOLOR,
+              backgroundGradientTo: colors.PRIMARYCOLOR,
+              decimalPlaces: 0,
+              color: (opacity = 255) => "black",
+            }}
+            style={{ marginTop: "10%", marginRight: 5, borderRadius: 10 }}
+          />
+          <Text
+            style={{
+              textAlign: "center",
+              marginBottom: "8%",
+              fontSize: 8,
+              fontWeight: "bold",
+            }}
+          >
+            {" "}
+            Day{" "}
+          </Text>
         </View>
         <View>
-        <Text style = {styles.analyticstext}>Most active on:  <Stat name={backendvalue1} /></Text>
-        <Text style = {styles.analyticstext}>Today's activity: <Stat name={backendvalue2}/></Text>
-        <Text style = {styles.analyticstext}>This week's activity: <Stat name={backendvalue3}/></Text>
-      </View>
-      <Pressable
-          style={styles.button}
+          <Text style={AnalyticsScreenStyles.analyticstext}>
+            Most active on: <Stat name={backendvalue1} />
+          </Text>
+          <Text style={AnalyticsScreenStyles.analyticstext}>
+            Today's activity: <Stat name={backendvalue2} />
+          </Text>
+          <Text style={AnalyticsScreenStyles.analyticstext}>
+            This week's activity: <Stat name={backendvalue3} />
+          </Text>
+        </View>
+        <Pressable
+          style={AnalyticsScreenStyles.button}
           onPress={() => navigation.navigate("LogSport")}
         >
-          <Text style={styles.text}>
-            {"BACK"}
-          </Text>
+          <Text style={AnalyticsScreenStyles.text}>{"BACK"}</Text>
         </Pressable>
       </View>
-      </>
-    )
+    </ScrollView>
+  );
 }
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: PRIMARYCOLOR,
-    },
-    input: {
-      marginHorizontal: "5%",
-      marginTop: "5%",
-      padding: "3%",
-      borderWidth: 0.5,
-      borderColor: 'grey',
-      borderRadius: 10,
-      flexDirection: "row",
-      justifyContent: "space-between",
-    },
-    button: {
-      backgroundColor: CARDCOLOR,
-      marginHorizontal: "5%",
-      marginTop: "5%",
-      marginBottom: "20%",
-      padding: "3%",
-      borderRadius: 10,
-      justifyContent: "center",
-    },
-    text: {
-      fontWeight: "bold", 
-      textAlign: "center", 
-      textTransform: 'uppercase'
-    },
-    analyticstext: {
-      marginBottom: "4%",
-      marginLeft: "5%",
-    }
-  });
+const styles = StyleSheet.create({});
